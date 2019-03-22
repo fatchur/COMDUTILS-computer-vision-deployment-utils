6f10eec8dade10a71ad851f9c720c7ad084e4d18
root <fatchur.rahman1@gmail.com>
2019-03-22 07:12:32 +0000

    completing the package requirements

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='comdutils',  # Required
-    version='v0.0.1',  # Required
+    version='v0.1',  # Required
     description='a simple package for computer vision deployment',  # Required
     long_description="a simple package for computer vision deployment",  # Optional
     author='Chur Chur',  # Optional
